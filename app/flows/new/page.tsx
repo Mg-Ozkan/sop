8c63f1f5b6f205770ca43009c9287ccd3eb83ebf
MGÖzkan <mg.ozkan@outlook.com>
2024-11-03 00:06:37 +0100

    Added example css structer inside api/flows/new component.

--- app/flows/new/page.tsx
+++ app/flows/new/page.tsx
@@ -1,1 +1,12 @@
 //Create new view
+import React from 'react'
+
+import styles from "./css/page.module.scss";
+
+export default function NewPage() {
+    return (
+        <div className={styles.h1}>
+            <h1> Create new flow </h1>
+        </div>
+    )
+}
