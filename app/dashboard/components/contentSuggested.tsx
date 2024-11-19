43ed302d58d7633cbed64d79fcb20ba6e46c6620
Ramon van Creij <creijr@kempel.nl>
2024-11-19 12:05:39 +0100

    Updated new project to redirect to flows. Added hover over pointer. Fixed scaling issue with carousel container.

--- app/dashboard/components/contentSuggested.tsx
+++ app/dashboard/components/contentSuggested.tsx
@@ -1,9 +1,18 @@
 import ProjectFetcher from './projectFetcher';
 import ProjectListing from './projectListing';
 import styles from "../page.module.scss";
+import { useRouter } from 'next/navigation';
+
+
+
 
 export default function ContentSuggested() {
 	const fetchReturn = ProjectFetcher({userId: 1, fetchMostRecent: true});
+	const router = useRouter();
+
+	const navigateToProject = () => {
+		router.push('/flows')
+	}
 
 	return (
 		<div className={styles["suggested-container"]}>
@@ -12,7 +21,7 @@ export default function ContentSuggested() {
 			</h1>
 			<div className={styles["suggested-wrapper"]}>
 				<div className={styles["suggested-actions"]}>
-					<div className={styles["new-project-container"]}>
+					<div className={styles["new-project-container"]} onClick={navigateToProject}>
 						<div className={styles["horizontal-plus"]} />
 						<div className={styles["vertical-plus"]} />
 						<p className={styles["new-project-title"]}>
--- app/dashboard/components/slider.tsx
+++ app/dashboard/components/slider.tsx
@@ -1,5 +1,8 @@
 import Carousel from "react-multi-carousel";
 import ProjectListing from "./projectListing"
+
+import { ButtonGroupProps, ArrowProps, DotProps } from 'react-multi-carousel/lib/types';
+
 import "react-multi-carousel/lib/styles.css";
 import "./slider.css";
 
@@ -18,14 +21,32 @@ const responsive = {
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
         items: 4,
-        slidesToSlide: 1 // optional, default to 1.
+        slidesToSlide: 1,
+        partialVisibilityGutter: 40
     },
+};
+
+
+interface CarouselButtonGroupProps extends ButtonGroupProps {
+    className?: string;
+}
+
+const CarouselButtonGroup = ({ next, previous }:CarouselButtonGroupProps) => {
+    return (
+      <div>
+            <button className="custom-arrow-left button" onClick={() => previous!()} />
+            <button className="custom-arrow-right button" onClick={() => next!()} />
+      </div>
+    );
 };
 
 export default function Slider({ projects }: SliderProps) {
     return (
         <div className="slider-wrapper">
             <Carousel
+                arrows={false}
+                renderButtonGroupOutside={true}
+                customButtonGroup={<CarouselButtonGroup />}
                 responsive={responsive}
                 partialVisible={true}
                 containerClass="react-multi-carousel-list"
