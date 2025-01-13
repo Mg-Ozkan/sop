9b3b364c406fe5e7afc991c594431895fa65cb49
Meau <m.mighiss@fontys.nl>
2025-01-13 19:55:54 +0100

    werkend front end en backend server zonder relatie

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,20 @@
-//import express, { Application } from 'express';
-//import sopRoutes from './routes/sopRoutes';
+import express, { Application } from 'express';
+import sopRoutes from './routes/sopRoutes';
 
-//const app: Application = express();
+const app: Application = express();
+
+import cors from "cors";
+app.use(cors());
 
 // Middleware voor JSON-parsing
-//app.use(express.json());
+app.use(express.json());
 
 // Prefix voor SOP-routes
-//app.use('/api/sop', sopRoutes);
+app.use('/api/sop', sopRoutes);
 
+console.log("Starting backend...");
 // Start server
-//const PORT = 6000; // De backend poort
-//app.listen(PORT, () => {
-  //console.log(`Server running on http://localhost:${PORT}`);
-//});
+const PORT = 8000; // De backend poort
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
